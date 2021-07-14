e8d160488cb8ac9bf33edd85a77a5835e71290a1
idiliev18 <idiliev18@codingburgas.bg>
2021-07-14 22:17:52 +0300

    Implement table sorting by columns
    * Add header object
    * Add sortEmployees()

--- internship-client/src/app/components/table-css/table-css.component.ts
+++ internship-client/src/app/components/table-css/table-css.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeesService } from 'src/app/services/employees.service';
 import { Employee } from 'src/app/Employee';
+import { keyframes } from '@angular/animations';
 
 @Component({
     selector: 'app-table-css',
@@ -9,7 +10,71 @@ import { Employee } from 'src/app/Employee';
 })
 export class TableCssComponent implements OnInit {
     employees: Employee[] = [];
+
+    header = {
+        id: 'Id↑', //↑
+        firstName: 'First name',
+        surname: 'Surname',
+        lastName: 'Last name',
+        gender: 'Gender',
+        age: 'Age',
+        salary: 'Salary',
+        position: 'Position',
+        yearOfAppointment: 'Year of appointment',
+        totalHolidays: 'Total holidays',
+        remainingHolidays: 'Remaining holidays',
+        sort: 'id',
+        order: '↑'
+    };
+
     constructor(private employeeService: EmployeesService) {}
+
+    sortEmployees(field: string) {
+
+        // Checks if there are elements in the array
+        if (this.employees.length == 0) {
+            return;
+        }
+
+        if (this.header.sort != field) {
+            this.header.order = '↓';
+        }
+
+        let e = this.employees[0] as any;
+
+        (this.header as any)[this.header.sort] = (this.header as any)[this.header.sort].slice(0, -1);
+        this.header.sort = field;
+
+        if (typeof e[field] === 'string') {
+            if (this.header.order == '↑') {
+                this.employees.sort((a, b) =>
+                    (b as any)[field].localeCompare((a as any)[field])
+                );
+
+                this.header.order = '↓';
+            } else {
+                this.employees.sort((a, b) =>
+                    (a as any)[field].localeCompare((b as any)[field])
+                );
+
+                this.header.order = '↑';
+            }
+        } else {
+            if (this.header.order == '↑') {
+                this.employees.sort(
+                    (a, b) => (b as any)[field] - (a as any)[field]
+                );
+                this.header.order = '↓';
+            } else {
+                this.employees.sort(
+                    (a, b) => (a as any)[field] - (b as any)[field]
+                );
+                this.header.order = '↑';
+            }
+        }
+
+        (this.header as any)[field] = (this.header as any)[field] + this.header.order;
+    }
 
     deleteEmployee(id: any) {
         this.employeeService.deleteEmployee(id).subscribe();
